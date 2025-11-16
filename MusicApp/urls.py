0205b24b20771ba87943b571447dd6f3e204890d
Jagadeesh <koppisettijagadeesh123@gmail.com>
2025-11-16 20:27:11 +0530

    Add django-cors-headers

--- MusicApp/urls.py
+++ MusicApp/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from music import views
+from MusicApp.music import views
 from django.conf import settings
 from django.conf.urls.static import static
 
